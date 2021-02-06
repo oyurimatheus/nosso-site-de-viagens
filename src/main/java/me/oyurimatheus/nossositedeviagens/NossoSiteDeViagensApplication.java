2b6699d62509619cb713be01165fc73ad5121751
Yuri Matheus <yurimatheusb@hotmail.com>
2021-02-06 12:51:14 -0300

    inicia o projeto

---
+++ src/main/java/me/oyurimatheus/nossositedeviagens/NossoSiteDeViagensApplication.java
@@ -0,0 +1,13 @@
+package me.oyurimatheus.nossositedeviagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoSiteDeViagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoSiteDeViagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/oyurimatheus/nossositedeviagens/NossoSiteDeViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package me.oyurimatheus.nossositedeviagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoSiteDeViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
